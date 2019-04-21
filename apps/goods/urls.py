53391a2bc2ad0896cde43511e3a4da388d548ca8
IfanTsai <1558499691@qq.com>
2019-04-21 15:13:37 +0800

    添加列表页

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from apps.goods.views import IndexView, DetailView
+from apps.goods.views import IndexView, DetailView, ListView
 
 urlpatterns = [
     url("^index$", IndexView.as_view(), name='index'),
     url("^goods/(?P<goods_id>\d+)$", DetailView.as_view(), name='detail'),
+    url("^list/(?P<type_id>\d+)/(?P<page>\d+)$", ListView.as_view(), name='list'),
 ]
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -4,6 +4,7 @@ from django.views.generic import View
 from django_redis import get_redis_connection
 from django.core.cache import cache
 from apps.order.models import OrderGoods
+from django.core.paginator import Paginator
 from apps.goods.models import (
     GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner, GoodsSKU,
 )
@@ -68,7 +69,7 @@ class DetailView(View):
             return redirect(reverse('goods:index'))
 
         # 获取商品分类信息
-        types = GoodsType.objects.all()
+        goods_types = GoodsType.objects.all()
 
         # 获取商品评论信息
         sku_orders = OrderGoods.objects.filter(sku=sku).exclude(comment='')
@@ -76,13 +77,16 @@ class DetailView(View):
         # 获取新品信息
         new_skus = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')[:2]
 
+        # 获取同一个SPU的其他规格商品
+        same_spu_skus = GoodsSKU.objects.filter(goods=sku.goods).exclude(id=goods_id)
+
         # 判断用户是否登陆，来决定是否获取用户购物车中商品的数量
         user = request.user
         cart_goods_count = 0
         if user.is_authenticated():
             conn = get_redis_connection('default')
             cart_key = 'cart_%d' % user.id
-            art_goods_count = conn.hlen(cart_key)
+            cart_goods_count = conn.hlen(cart_key)
 
             # 添加用户浏览记录
             conn = get_redis_connection('default')
@@ -96,10 +100,92 @@ class DetailView(View):
 
         context = {
             'sku': sku,
-            'types': types,
+            'goods_types': goods_types,
             'sku_orders': sku_orders,
             'new_skus': new_skus,
             'cart_goods_count': cart_goods_count,
+            'same_spu_skus': same_spu_skus,
         }
 
         return render(request, 'detail.html', context)
+
+class ListView(View):
+    """
+    列表页视图
+    """
+    def get(self, request, type_id, page):
+
+        # 获取种类信息
+        try:
+            goods_type = GoodsType.objects.get(id=type_id)
+        except GoodsType.DoesNotExist:
+            return redirect(reverse('goods:index'))
+
+        # 获取商品分类信息
+        goods_types = GoodsType.objects.all()
+
+        # 获取排序方式
+        sort_type = request.GET.get('sort', 'default')
+        sort = {
+            'default': '-id',
+            'price': 'price',
+            'hot': '-sales',
+        }.get(sort_type, '-id')
+
+        # 获取分类商品信息
+        skus = GoodsSKU.objects.filter(type=goods_type).order_by(sort)
+
+        # 对数据进行分页
+        paginator = Paginator(skus, 1)
+
+        try:
+            page = int(page)
+        except Exception:
+            page = 1
+
+        if page > paginator.num_pages:
+            page = 1
+
+        # 获取第page页的Page实例对象
+        skus_page = paginator.page(page)
+
+        # 页码控制，页面上最多显示5个页码
+        # 获取总页数
+        num_pages = paginator.num_pages
+        # 总页数小于5，页面上显示所有页码
+        if num_pages < 5:
+            pages = range(1, num_pages + 1)
+        # 下面是总页数大于5页的情况
+        # 当前页是前三页，显示前5页
+        elif page <= 3:
+            pages = range(1, 6)
+        # 当前页是后三页，显示后5页
+        elif num_pages - page <= 2:
+            pages = range(num_pages - 4, num_pages + 1)
+        # 其他情况，显示当前页的前2页和当前页的后2页
+        else:
+            pages = range(page - 2, page + 3)
+
+
+        # 获取新品信息
+        new_skus = GoodsSKU.objects.filter(type=goods_type).order_by('-create_time')[:2]
+
+        # 判断用户是否登陆，来决定是否获取用户购物车中商品的数量
+        user = request.user
+        cart_goods_count = 0
+        if user.is_authenticated():
+            conn = get_redis_connection('default')
+            cart_key = 'cart_%d' % user.id
+            cart_goods_count = conn.hlen(cart_key)
+
+        context = {
+            'goods_type': goods_type,
+            'goods_types': goods_types,
+            'skus_page': skus_page,
+            'pages': pages,
+            'new_skus': new_skus,
+            'cart_goods_count': cart_goods_count,
+            'sort': sort_type,
+        }
+
+        return render(request, 'list.html', context)
